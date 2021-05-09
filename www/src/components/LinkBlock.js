05a3b8d4cab81eb7e98ec2abbb0708c8ffdbd2a9
ahnl <46541386+ahnl@users.noreply.github.com>
2021-05-09 16:41:17 +0300

    Make LinkBlocks a links

--- www/src/components/LinkBlock.js
+++ www/src/components/LinkBlock.js
@@ -1,12 +1,12 @@
 export function LinkBlock({data}) {
-    const {title, icon, siteName, date} = data;
+    const {title, icon, siteName, date, url} = data;
     return (
-        <div className="linkBlock">
+        <a href={url} className="linkBlock" target="_blank" rel="noreferrer">
             <span>{title} {date ? <span className="date">{date}</span> : ''}</span>
             <span>
                 <img src={icon} alt={siteName} />
                 {siteName}
             </span>
-        </div> 
+        </a> 
     );
 }
